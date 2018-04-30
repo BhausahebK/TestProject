f1d8c420d31715931a8d6ec04302cfbf3e09aadc
BhausahebK <38851537+BhausahebK@users.noreply.github.com>
2018-04-30 14:56:27 +0530

    new code

--- TestProject/src/Test.java
+++ TestProject/src/Test.java
@@ -1,4 +1,18 @@
 
 public class Test {
+Webdriver driver;
 
+  @Test
+  public void login(){
+    
+  driver = new firefoxdriver();
+    
+    driver.get("https://www.firstcry.com");
+  
+  
+  
+  
+  
+  
+  
 }
